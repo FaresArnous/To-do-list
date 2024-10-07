3af862f01fdc6cffa87a5ff83ef20f7d309c9cba
fares Arnous <Faris.Arnous@hotmail.com>
2024-10-07 14:34:27 +0300

    added some tasks form to the task page.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.static("public")); //serving static files
 
+app.use(express.urlencoded({ extended: false })); //middleware to exratc incoming data
+
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "home.html");
   res.sendFile(htmlFilePath);
@@ -16,6 +18,9 @@ app.get("/about", function (req, res) {
 app.get("/tasks", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "tasks.html");
   res.sendFile(htmlFilePath);
+});
+app.post("/tasks", function (res, req) {
+  req.body;
 });
 
 app.listen(9090);
