967b1529c814db9ab2ae17a92944508c31a60f9b
fares Arnous <Faris.Arnous@hotmail.com>
2024-10-24 10:05:19 +0300

    change the routers and added some qures, added delete and edit buttons

--- routes/tasks.js
+++ routes/tasks.js
@@ -15,11 +15,34 @@ router.get("/about", function (req, res) {
 });
 
 router.get("/tasks", async function (req, res) {
+  const [result] = await db.query(
+    "SELECT COUNT(*) AS totalTasks FROM tasks.task"
+  );
+  const numberOfTasks = result[0].totalTasks;
+
   const [tasks] = await db.query("SELECT * FROM tasks.task");
-  res.render("tasks", { tasks: tasks });
+
+  res.render("tasks", { tasks: tasks, numberOfTasks: numberOfTasks });
+
+  const formattedTasks = tasks.map((task) => {
+    task.dueDate = new Date(task.date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return task;
+  });
+
+  res.render("tasks", { tasks: formattedTasks });
 });
 
-router.post("/tasks", function (req, res) {
+router.post("/tasks", async function (req, res) {
+  const { name, description, dueDate } = req.body;
+  await db.query(
+    "INSERT INTO tasks.task (title, content, date) VALUES (?, ?, ?)",
+    [name, description, dueDate]
+  );
+
   res.redirect("/confirm");
 });
 
